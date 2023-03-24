e87dba84776bc2eb3286b1f5f14e25200ef6a31a
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-03-24 12:12:04 +0000

    feat: wait related API

---
+++ wait.go
@@ -0,0 +1,60 @@
+package batchq
+
+import (
+	"errors"
+	"github.com/KevinZonda/batchq/job"
+	"time"
+)
+
+func (q *BatchQ[T]) WaitJobResult(hash string, interval time.Duration) job.JobResult[T] {
+	found := false
+	var result job.JobResult[T]
+
+startCheck:
+	found, result = q.Check(hash)
+	if found {
+		return result
+	}
+	time.Sleep(interval)
+	goto startCheck
+}
+
+func (q *BatchQ[T]) WaitResult(hash string, interval time.Duration) (result T, err error) {
+	jr := q.WaitJobResult(hash, interval)
+	if jr.Ok() {
+		return jr.Value(), nil
+	}
+	return result, jr.Error()
+}
+
+func (q *BatchQ[T]) WaitJobResultChain(hash string, interval time.Duration) <-chan job.JobResult[T] {
+	ch := make(chan job.JobResult[T])
+	go func() {
+		ch <- q.WaitJobResult(hash, interval)
+		close(ch)
+	}()
+	return ch
+}
+
+var ErrTimeout = errors.New("timeout")
+
+func (q *BatchQ[T]) WaitJobResultWithTimeout(hash string, interval time.Duration) (result job.JobResult[T], err error) {
+	select {
+	case result = <-q.WaitJobResultChain(hash, interval):
+		return
+	case <-time.After(5 * time.Second):
+		err = ErrTimeout
+	}
+	return
+}
+
+func (q *BatchQ[T]) WaitResultWithTimeout(hash string, interval time.Duration) (result T, err error) {
+	jr, err := q.WaitJobResultWithTimeout(hash, interval)
+	if err != nil {
+		return
+	}
+	if jr.Ok() {
+		return jr.Value(), nil
+	}
+	return result, jr.Error()
+}
